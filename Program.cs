7b011a03a55e71bca84394cd1a332572b4f2ba3e
Harley Rogers <harleydev512@gmail.com>
2020-06-19 16:21:15 -0500

    added some notes to my code

--- Program.cs
+++ Program.cs
@@ -9,39 +9,39 @@ namespace storytest2
     {
         static void Main(string[] args)
         {
+            //These are the prompts to get the veraibles for the paragraph
+             Console.WriteLine("What is your name?");//asks the Question
+             string name = Console.ReadLine();//lets the user input their value
 
-             Console.WriteLine("What is your name?");//1
-             string name = Console.ReadLine();
-
-             Console.WriteLine("What is your pronoun? (He or She)");//2
+             Console.WriteLine("What is your pronoun? (He or She)");
              string gender = Console.ReadLine();
 
-             Console.WriteLine("What is your profession?");//3
+             Console.WriteLine("What is your profession?");
              string pro = Console.ReadLine();
 
-             Console.WriteLine("What do you want to become?");//4
+            Console.WriteLine("What do you want to become?");
              string become = Console.ReadLine();
 
-             Console.WriteLine("What is your least favorate animal?");//5
+             Console.WriteLine("What is your least favorate animal?");
              string animal = Console.ReadLine();
 
              Console.WriteLine("Put your number down.");//math
-             int num1 = int.Parse(Console.ReadLine());
-
+             int num1 = int.Parse(Console.ReadLine());/*need to do int.Parse to change it from 
+            a string to an integer*/
              Console.WriteLine("Put your second number down.");//math
-             int num2 = int.Parse(Console.ReadLine());
+             int num2 = int.Parse(Console.ReadLine());//same here
 
-             Console.WriteLine("What is your favorate food?");//6
+             Console.WriteLine("What is your favorate food?");//just another prompt
              string food = Console.ReadLine();
 
-             int answer = Sum(num1, num2);
+             int answer = Sum(num1, num2);//this is the output of Method #1
 
-             //challenge
+             //Bellow is the paragraph(output)
 
              Console.WriteLine($"This is a story about {name}. {gender} is a {pro}. {gender} wants" +
-                 $"to become a {become}, but {gender} will probably be a {animal} instead. As you can see {gender} is" +
+                 $" to become a {become}, but {gender} will probably be a {animal} instead. As you can see {gender} is" +
                  $" no math genious. {gender} thinks {num1} plus {num2} equils {food}, but is really {answer}. ");
-
+            // here we have more prompts
              Console.WriteLine("Do you know math?");
              string isTrue = Console.ReadLine();
 
@@ -57,18 +57,18 @@ namespace storytest2
              Console.WriteLine("What is something you hate?");
              string badName = Console.ReadLine();
 
-             int answer2 = Mul(num3, num4);
-
+             int answer2 = Mul(num3, num4);//this is the output of method #2
+            //another paragraph(output)
              Console.WriteLine($"Computer used {name} has said {isTrue} to being good at math. " +
                  $"{gender} says {num3} * {num4} = {wrongAn}. But it is {answer2}. {gender} is a {badName}.");
 
-
-            Console.WriteLine("How many numbers do you want to add together?");
-            int userAmount = int.Parse(Console.ReadLine());
+            //start here is for the section using array type methods and some loops
+            Console.WriteLine("How many numbers do you want to add together?");//this asks the user 
+            int userAmount = int.Parse(Console.ReadLine());//this is where the user inputs
 
             int[] numbers = new int[userAmount];
 
-            for (int i = 0; i < userAmount; i++)
+            for (int i = 0; i < userAmount; i++)//this is the for loop
             {
                 Console.WriteLine("Enter a number.");
                 int userNumber = int.Parse(Console.ReadLine());
@@ -78,13 +78,13 @@ namespace storytest2
             }
 
             int sumAll = Add(numbers);
-            Console.WriteLine(sumAll);
-        }
-         public static int Sum(int num1, int num2)
+            Console.WriteLine(sumAll);//this outputs the answer.
+        }//this is the end of that method, array, loop thingy. 
+         public static int Sum(int num1, int num2) //mehtod #1
          {
              return num1 + num2;
          }
-         public static int Mul(int num3, int num4)
+         public static int Mul(int num3, int num4) //method #2
          {
              return num3 * num4;
          }
@@ -92,7 +92,7 @@ namespace storytest2
     
         
 
-            public static int Add(params int[] args)//[1,2,3,4,5,6...]
+            public static int Add(params int[] args)//[1,2,3,4,5,6...] Method #3
             {
                 int addit = 0;
 
